4622d9c8cbebb91e6ec590804053e4e219100546
duniksergei <dunik99@mail.ru>
2018-04-24 17:05:09 +0300

    second commit

---
+++ praktika1/praktika1/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktika1
+{
+    class Student
+    {
+        
+        public string Name { get; set; }
+        public int Kurs { get; set; }
+        public bool Sex { get; set; }
+        public Student()  //конструктор по умолчанию
+        {
+            Name = null;
+            Kurs = 0;
+            Sex = true;
+        }
+        
+        
+        public virtual void Display() //метод для вывода 
+        {
+            Console.WriteLine("Имя студента: " + Name);
+            Console.WriteLine("Курс: " + Kurs);
+            Console.WriteLine("Пол: " + Sex);
+        }
+        public virtual void Input()  //метод для ввода
+        {
+            Console.WriteLine("Имя студента:  ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Курс: ");
+            Kurs = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Пол: ");
+            Sex = Convert.ToBoolean(Console.ReadLine());
+
+        }
+
+    }
+}
