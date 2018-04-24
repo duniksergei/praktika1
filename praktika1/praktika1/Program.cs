a6fa0b3544458a54ee69ee327d41fc06369cdc83
duniksergei <dunik99@mail.ru>
2018-04-24 17:41:01 +0300

    third commit

--- praktika1/praktika1/Program.cs
+++ praktika1/praktika1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace praktika1
 {
@@ -10,6 +6,28 @@ namespace praktika1
     {
         static void Main(string[] args)
         {
+
+
+            //вывод информации в консоль
+            Console.WriteLine("---------------------------------------------- ");
+            Console.WriteLine("Введите информацию о Студенте: ");
+            Console.WriteLine("---------------------------------------------- ");
+            Student st1 = new Student(); //создание объекта класса
+            st1.Input();      //вызов метода ввода класса Student
+
+            Console.Clear(); //очищение консоли
+
+            //вывод информации в консоль
+            Console.WriteLine("----------------------------------------------- ");
+            st1.Display();     //вызов метода вывода класса Student
+            Console.WriteLine("----------------------------------------------- ");
+
+            Console.ReadKey();  //ожидание нажатия клавиши для завершение программы
+
+
+
+
+
         }
     }
 }
